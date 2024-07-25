911ba3c8802ce5b6fbff24f8f11d5f14583dbd8b
Jennifer Zhan <jennifer.zhan.2015@gmail.com>
2024-07-25 11:10:30 -0400

    finish the home page style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Templates from "./component/Templates/Templates";
 import HomePage from "./pages/HomePage";
+import Login from "./component/Login/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/template" element={<Templates />}></Route>
           <Route
             path="/"
